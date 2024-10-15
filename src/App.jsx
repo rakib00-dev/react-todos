745f4f31d5da9781f2bc3b062f27fc4d1723ca24
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-10-15 16:50:47 +0600

    added all files of the app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Todo from './components/Todo';
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return <Todo></Todo>;
+}
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import Swal from 'sweetalert2/dist/sweetalert2.all.js';
+
+function Todo() {
+  const [tasks, setTask] = useState(['33', 344, 'e34']);
+  const [newTask, setNewTask] = useState('');
+
+  function handelEvent(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() == '') {
+      Swal.fire('Please Write Someting!');
+    } else {
+      setTask((t) => [...tasks, newTask]);
+      setNewTask('');
+      localStorage.setItem('', [...tasks]);
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTask = tasks.filter((_, i) => i !== index);
+    setTask(updatedTask);
+  }
+
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTask = [...tasks];
+      [updatedTask[index], updatedTask[index - 1]] = [
+        updatedTask[index - 1],
+        updatedTask[index],
+      ];
+      setTask(updatedTask);
+    }
+  }
+
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTask = [...tasks];
+      [updatedTask[index], updatedTask[index + 1]] = [
+        updatedTask[index + 1],
+        updatedTask[index],
+      ];
+      setTask(updatedTask);
+      console.log([updatedTask[index], updatedTask[index + 1]]);
+    }
+  }
+  return (
+    <div id="wrapper">
+      <div className="input-group mb-3">
+        <input
+          id="input"
+          type="text"
+          className="form-control"
+          placeholder="Write Toods"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          value={newTask}
+          onChange={handelEvent}
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+          onClick={addTask}
+        >
+          Add
+        </button>
+      </div>
+      <ol id="list-group" className="list-group">
+        {tasks?.map((task, index) => (
+          <li key={index} className="list-group-item">
+            <span>{task}</span>
+            <span>
+              <button id="delete-btn" onClick={() => deleteTask(index)}>
+                Delete
+              </button>
+              <button id="move-btn" onClick={() => moveTaskUp(index)}>
+                ⬆️
+              </button>
+              <button id="move-btn" onClick={() => moveTaskDown(index)}>
+                ⬇️
+              </button>
+            </span>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default Todo;
